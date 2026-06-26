e248b172dcdf7a425d656e17e175fd80f5ef4fff
Backmann <backmannleo@gmail.com>
2026-06-26 23:02:06 +0200

    fix(cart-locations): wire Responsibility repo + ResponsibilityGuard into module

--- src/cart-locations/cart-locations.module.ts
+++ src/cart-locations/cart-locations.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CartLocation } from '../entities/cart-location.entity';
+import { Responsibility } from '../entities/responsibility.entity';
 import { CartLocationsService } from './cart-locations.service';
 import { CartLocationsController } from './cart-locations.controller';
+import { ResponsibilityGuard } from '../common/guards/responsibility.guard';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CartLocation])],
+  imports: [TypeOrmModule.forFeature([CartLocation, Responsibility])],
   controllers: [CartLocationsController],
-  providers: [CartLocationsService],
+  providers: [CartLocationsService, ResponsibilityGuard],
   exports: [CartLocationsService],
 })
 export class CartLocationsModule {}
